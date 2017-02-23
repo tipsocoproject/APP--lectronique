52b8c1d7a659960462ad57f19a476e06366f94a7
FengDavid96 <fyhdavid@gmail.com>
2017-02-23 14:17:05 +0100

    QA,C,D

---
+++ P1questions.m
@@ -0,0 +1,116 @@
+%Données du signal
+Fe = 16000;            %fq d'échantillonnage
+A = 2;                 %amplitude en Volt
+F0 = 1000;             %fq du signal en Hz
+T0 = 1/F0;             %période du signal
+P = 5;                 %nb de périodes
+S = 5*T0;              %5 fois la période
+Te = 1/Fe;             %le pas d'échantillonnage
+t = 0:Te:S;            %intervalle de temps avec 1/FS le pas d'échantillonnage
+
+
+Y1 = A*sin(2*pi*F0*t); %signal continu
+sum(Y1)
+%QUESTION A
+figure(1)
+plot(t,Y1);
+xlabel('seconds');
+ylabel('Volts');
+title('Signal in the domain - 5 periods');
+
+
+%QUESTION C partie 1
+n = Fe/F0;             %nb d'échantillons sur une période du signal
+syms k;                %déclarer la variable k
+C = 1/(n-0+1);         %facteur dans l'approximation de Riemann
+
+Y2 = A*sin(2*pi*F0*k*Te); %Signal sinusoïdal discret
+
+%Calcul valeur moyenne
+ValMoyY2 = C*symsum(Y2, k, 0, n) %valeur moyenne estimée du signal Y2 sur une période
+VMY2 = C*sum(Y1)
+%Calcul puissance moyenne
+PowerY2 = C*symsum((Y2)^2, k, 0,  n) %Puissance moyenne estimée du signal Y2 sur une période
+K=Y1.*Y1;
+PY2 = C*sum(K)
+
+%Question C partie 2
+Fe1 = 20000;                %nouvelle fréquence d'échantillonnage
+Te1 = 1/Fe1;                %nouveau pas d'échantillonnage
+n1 = Fe1/F0;                %nb d'échantillons sur une période du signal
+Y3 = A*sin(2*pi*F0*k*Te1);  %Signal sinusoïdal discret
+
+%Calcul valeur moyenne
+ValMoyY3 = C*symsum(Y3, k, 0, n)     %valeur moyenne estimée du signal Y3 sur une période
+
+%Calcul puissance moyenne
+PowerY3 = C*symsum((Y3)^2, k, 0, n1) %Puissance moyenne estimée du signal Y3 sur une période
+
+%Malgré une augmentation de la fréquence d'échantillonnage, la puissance
+%moyenne du signal reste la même
+
+
+
+%Question D partie 1 : fonction max
+%Affichage
+figure(2)
+Y1t=max(Y1,0);
+plot(t,Y1t);
+xlabel('seconds');
+ylabel('Volts');
+title('Signal in the domain - 5 periods');
+
+%Calcul valeur moyenne
+ValMoyY1t = C*symsum(Y1t, k, 0, n);  %valeur moyenne estinée du signal Y1t à chaque k
+ResultValMoyY1t = mean(ValMoyY1t)    %valeur moyenne estimée du signal Y1t sur une période
+
+%Calcul puissance moyenne
+Y1tbis = Y1t.*Y1t;                    %Y1t au carré
+PowerY1t = C*symsum(Y1tbis, k, 0, n); %Puissance moyenne estimée du signal Y1t à chaque k
+ResultPowerY1t = mean(PowerY1t)       %Puissance moyenne estimée du signal Y1t sur une période
+
+
+
+
+%Question D partie 2 : fonction abs
+%Affichage
+figure(3)
+Y4 = abs(Y1);
+plot(t,Y4);
+xlabel('seconds');
+ylabel('Volts');
+title('Signal in the domain - 5 periods');
+
+%Calcul valeur moyenne
+ValMoyY4 = C*symsum(Y4, k, 0, n);    %valeur moyenne estimée du signal Y4 à chaque k
+ResultValMoyY4 = mean(ValMoyY4)      %valeur moyenne estimée du signal Y4 sur une période
+
+%Calcul puissance moyenne
+Y4bis = Y4.*Y4;                     %Y4 au carré
+PowerY4 = C*symsum(Y4bis, k, 0, n); %Puissance moyenne estimée du signal Y4 à chaque k
+ResultPowerY4 = mean(PowerY4)       %Puissance moyenne estimée du signal Y4 sur une période
+
+
+%Question D partie 3 : fonction square
+%Affichage
+figure(4)
+Y5t = 2*square(2*pi*F0*t);
+Y5 = max(Y5t,0.5);
+plot(t,Y5);
+xlabel('seconds');
+ylabel('Volts');
+title('Signal in the domain - 5 periods');
+
+%Calcul valeur moyenne
+ValMoyY5 = C*symsum(Y5, k, 0, n);    %valeur moyenne estimée du signal Y5 à chaque k
+ResultValMoyY5 = mean(ValMoyY5)      %valeur moyenne estimée du signal Y5 sur une période
+
+%Calcul puissance moyenne
+Y5bis = Y5.*Y5;                     %Y5 au carré
+PowerY5 = C*symsum(Y4bis, k, 0, n); %Puissance moyenne estimée du signal Y5 à chaque k
+ResultPowerY5 = mean(PowerY5)       %Puissance moyenne estimée du signal Y5 sur une période
+
+
+
+
+%Question E
